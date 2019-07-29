2d0fa4a9e960176834e9f7e410aa49bcd66421d4
Benjamin Grelié <204627+synoptase@users.noreply.github.com>
2019-07-29 10:08:57 +0200

    feat: functionnal issue to md process

---
+++ index.js
@@ -0,0 +1,76 @@
+const { Toolkit } = require("actions-toolkit");
+const fs = require("fs");
+const fm = require("front-matter");
+const path = require("path");
+const nunjucks = require("nunjucks");
+const kebabCase = require("kebab-case");
+
+Toolkit.run(
+  async tools => {
+    const template = process.env.TEMPLATE_FILE || "./actions/template.md";
+    const contentPath = process.env.CONTENT_PATH || "./content/docs/";
+    const env = nunjucks.configure({ autoescape: false });
+
+    tools.log.debug("context", tools.context);
+
+    const {
+      repository: { issue }
+    } = await tools.github.graphql(
+      `query issue($owner: String!, $repo: String!, $number: Int!) {
+      repository(owner: $owner, name: $repo) {
+        issue(number: $number) {
+          title
+          number
+          author {
+            ... on User {
+              name
+            }
+          }
+          createdAt
+          body
+          labels(first: 10) {
+            totalCount
+            edges {
+              node {
+                name
+              }
+            }
+          }
+        }
+      }
+    }`,
+      {
+        owner: tools.context.payload.repository.owner.login,
+        repo: tools.context.payload.repository.name,
+        number: tools.context.payload.issue.number
+      }
+    );
+
+    // Convert the issue's body to a front matter JSON
+    const { attributes, body } = fm(issue.body);
+    tools.log.info(`Front matter for issue #${issue.number} is`, attributes);
+
+    // Create the .md file
+    var templateContent = fs.readFileSync(template, "utf8");
+    const templated = env.renderString(templateContent, {
+      body,
+      title: issue.title,
+      createdAt: issue.createdAt,
+      tags:
+        issue.labels.totalCount > 0 &&
+        issue.labels.edges.map(label => label.node.name),
+      author: issue.author.name,
+      notes: attributes.notes,
+      source: attributes.source
+    });
+
+    // Commit file
+    const fileToCommit = path.join(contentPath, kebabCase(issue.title) + ".md");
+    const err = fs.writeFileSync(fileToCommit, templated);
+    tools.log.info(`${fileToCommit} created`);
+  },
+  {
+    event: ["issues"],
+    secrets: ["GITHUB_TOKEN"]
+  }
+);
